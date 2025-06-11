c7e2109efc142a5b390662d4e58a3c4859842ef0
Fidelisaboke <fidelisaboke@gmail.com>
2025-06-11 15:44:26 +0300

    fix: fix typo insert user statement execution

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -33,8 +33,8 @@ def register():
 
         if error is None:
             try:
-                db.execute("INSERT INTO user (username, password) VALUES (?, ?)"),
-                (username, generate_password_hash(password)),
+                db.execute("INSERT INTO user (username, password) VALUES (?, ?)",
+                (username, generate_password_hash(password))),
                 db.commit()
             except db.IntegrityError:
                 error = f"User {username} is already registered."
